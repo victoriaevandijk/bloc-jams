1f7a67e4057ac8f4d09d0b52f89be507cf5abf14
torievandijk <victoriaevandijk@gmail.com>
2016-10-16 12:46:21 +0000

    DOM Scripting: Album View Assignment

--- scripts/album.js
+++ scripts/album.js
@@ -28,6 +28,23 @@ var albumMarconi = {
     ]
 };
 
+var albumThird = {
+    name: 'Name',
+    artist: 'Artist',
+    label: 'Label',
+    year: '1909',
+    albumArtUrl: 'assets/images/album_covers/02.png',
+    songs: [
+        { name: 'Hello, Operator?', length: '1:01' },
+        { name: 'Ring, ring, ring', length: '5:01' },
+        { name: 'Fits in your pocket', length: '3:21' },
+        { name: 'Can you hear me now?', length: '3:14' },
+        { name: 'Wrong phone number', length: '2:15' }
+    ]
+};
+
+
+
 var createSongRow = function(songNumber, songName, songLength) {
     var template =
         '<tr class="album-view-song-item">'
@@ -39,6 +56,12 @@ var createSongRow = function(songNumber, songName, songLength) {
 
     return template;
 };
+
+var albumTitle = document.getElementsByClassName('album-view-title')[0];
+var albumArtist = document.getElementsByClassName('album-view-artist')[0];
+var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
+var albumImage = document.getElementsByClassName('album-cover-art')[0];
+var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
 
 var setCurrentAlbum = function(album) {
     var albumTitle = document.getElementsByClassName('album-view-title')[0];
@@ -61,4 +84,14 @@ var setCurrentAlbum = function(album) {
 
 window.onload = function() {
     setCurrentAlbum(albumPicasso);
+    
+    var albums = [albumPicasso, albumMarconi, albumThird];
+    var index = 1;
+    albumImage.addEventListener('click', function(event) {
+         setCurrentAlbum(albums[index]);
+         index++;
+         if (index == albums.length) {
+             index = 0;
+         }
+     });
 };
