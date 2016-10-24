53e875c07e97eb79b50689495d54da7e3212cba1
torievandijk <victoriaevandijk@gmail.com>
2016-10-24 00:02:02 +0000

    DOM Scripting: Play/Pause Part 2 Assignment

--- scripts/album.js
+++ scripts/album.js
@@ -59,16 +59,28 @@ var setCurrentAlbum = function(album) {
     }
 };
 
+var child = document.getElementsByClassName('album-view-title')[0];
+var noParent = document.querySelector('html');
 
 var findParentByClassName = function(element, targetClass) {
-    if (element) {
-        var currentParent = element.parentElement;
-        while (currentParent.className != targetClass && currentParent.className !== null) {
-            currentParent = currentParent.parentElement;
-        }
-        return currentParent;
-    }
-};
+     var currentParent = element.parentElement;
+     
+     if (currentParent) {
+         while (currentParent.className && currentParent.className != targetClass) {
+             currentParent = currentParent.parentElement;
+         }
+         
+         if (currentParent.className == targetClass) {
+             return currentParent;
+         } else {
+             alert("No parent with that class name found.");
+         }
+     } else {
+         alert("No parent found.");
+      }
+  };
+  
+findParentByClassName(child, 'album-view');
 
 var getSongItem = function(element) {
     switch (element.className) {
