04cf49bc0e2450c794ce8840127a335396476c21
torievandijk <victoriaevandijk@gmail.com>
2016-11-08 01:35:23 +0000

    jQuery: Next and Previous Buttons Revised

--- scripts/album.js
+++ scripts/album.js
@@ -42,13 +42,12 @@ var createSongRow = function(songNumber, songName, songLength) {
     
     var offHover = function(event) {
         var songNumberCell = $(this).find('.song-item-number');
-        var songNumber = parseInt($(this).attr('data-song-number'));
+        var songNumber = parseInt(songNumberCell.attr('data-song-number'));
         
         if (songNumber !== currentlyPlayingSongNumber) {
             songNumberCell.html(songNumber);
         }
     };
-    
     $row.find('.song-item-number').click(clickHandler);
     $row.hover(onHover, offHover);
     return $row;
